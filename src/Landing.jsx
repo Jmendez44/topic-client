ab173a961c50dd881ebac82344d55b726337bbfb
jmendez44 <jenuel.mendez@gmail.com>
2019-12-07 04:43:14 -0500

    landing page started

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export const Landing = () => {
   return (
-    <div>
-      Landing
+    <div className="landing-container">
+      <div className="landing-nav">
+        <div className="nav-logo">LOGO</div>
+        <ul className="nav-links">
+          <li>ABOUT</li>
+          <li>DONATE</li>
+          <li className="twitch-btn"> Connect with Twitch</li>
+        </ul>
+      </div>      
     </div>
   )
 }

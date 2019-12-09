34af6ef88a2f13d1ab6a76d13d86498483f4e494
jmendez44 <jenuel.mendez@gmail.com>
2019-12-08 22:26:58 -0500

    landing page basic design done

---
+++ src/Example.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import TopicList from './TopicList'
+
+export const Example = () => {
+
+
+
+  return (
+    <div className="example-container">
+      <div className="left-example">
+        <div className="avi"></div>
+      </div>
+      <div className="right-example">
+        <ul className="ex-topics">
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+          <li className="ex-topic"></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Example } from "./Example";
 
 export const Landing = () => {
   return (
-    <div className="landing-container">
+    <>
       <div className="landing-nav">
         <div className="nav-logo">TOPICKS</div>
         <ul className="nav-links">
@@ -14,11 +15,35 @@ export const Landing = () => {
           </li>
           <li>
             <a className="twitch-btn" href="">
+              <img
+                src={require("./assets/twitch-purple-logo-black-and-white.png")}
+                alt=""
+              />
               Connect with Twitch
             </a>
           </li>
         </ul>
       </div>
-    </div>
+      <div className="landing-container">
+        <div className="decription">
+          <h1>
+            Enhance the podcast experience <br /> other stuff here
+          </h1>
+          <div className="btns">
+            <button className="twitch">
+              
+              <img
+                src={require("./assets/twitch-purple-logo-black-and-white.png")}
+                alt=""
+              />
+              twitch
+            </button>
+            <button>google</button>
+          </div>
+        </div>
+        <Example />
+        {/* <div className="footer"></div> */}
+      </div>
+    </>
   );
 };

fe6207bea57e7016c66f7b08dbbf6fa8d1b2c7d7
jmendez44 <jenuel.mendez@gmail.com>
2020-05-27 17:18:47 -0400

    under construction page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,40 +6,14 @@ export const Landing = () => {
     <>
       <div className="landing-nav">
         <div className="nav-logo">TOPICKS</div>
-        <ul className="nav-links">
-          <li>
-            <a href="">ABOUT</a>
-          </li>
-          <li>
-            <a href="">DONATE</a>
-          </li>
-          <li>
-            <a className="twitch-btn" href="">
-              <img
-                src={require("./assets/twitch-purple-logo-black-and-white.png")}
-                alt=""
-              />
-              Connect with Twitch
-            </a>
-          </li>
-        </ul>
+        
       </div>
       <div className="landing-container">
         <div className="decription">
           <h1>
-            Enhance the podcast experience <br /> other stuff here
+            Under Construction....
           </h1>
-          <div className="btns">
-            <button className="twitch">
-              
-              <img
-                src={require("./assets/twitch-purple-logo-black-and-white.png")}
-                alt=""
-              />
-              twitch
-            </button>
-            <button>google</button>
-          </div>
+          
         </div>
         <Example />
         {/* <div className="footer"></div> */}
